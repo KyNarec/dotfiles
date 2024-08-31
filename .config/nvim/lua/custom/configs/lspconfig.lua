d8fa42b712344b1d12d1ff575fce71980453d0f2
Simon-Sonnleitner <jim48@gmx.de>
2024-08-31 16:41:07 +0200

    retry

---
+++ .config/nvim/lua/custom/configs/lspconfig.lua
@@ -0,0 +1,29 @@
+local config = require("plugins.configs.lspconfig")
+
+local on_attach = config.on_attach
+local capabilities = config.capabilities
+
+local lspconfig = require("lspconfig")
+
+local util = require "lspconfig/util"
+
+local servers = {
+  "pyright",
+  "ruff_lsp",
+}
+
+lspconfig.clangd.setup {
+  on_attach = function(client, bufnr)
+    client.server_capabilities.signatureHelpProvider = false
+    on_attach(client, bufnr)
+  end,
+  capabilities = capabilities,
+}
+
+for _, lsp in ipairs(servers) do
+  lspconfig[lsp].setup({
+    on_attach = on_attach,
+    capabilities = capabilities,
+    filetypes = {"python"},
+  })
+end
---
+++ .config/nvim/lua/custom/plugins.lua
@@ -0,0 +1,161 @@
+local cmp = require "cmp"
+
+local plugins = {
+
+  {
+    "nvim-neotest/nvim-nio",
+  },
+
+  {
+    "rcarriga/nvim-dap-ui",
+    dependencies = "mfussenegger/nvim-dap",
+    config = function()
+      local dap = require("dap")
+      local dapui = require("dapui")
+      dapui.setup()
+      dap.listeners.after.event_initialized["dapui_config"] = function()
+        dapui.open()
+      end
+      dap.listeners.before.event_terminated["dapui_config"] = function()
+        dapui.close()
+      end
+      dap.listeners.before.event_exited["dapui_config"] = function()
+        dapui.close()
+      end
+    end
+  },
+
+  {
+    "mfussenegger/nvim-dap",
+    config = function(_, opts)
+      require("core.utils").load_mappings("dap")
+    end
+  },
+
+  {
+    "mfussenegger/nvim-dap-python",
+    ft = "python",
+    dependencies = {
+      "mfussenegger/nvim-dap",
+      "rcarriga/nvim-dap-ui",
+      "nvim-neotest/nvim-nio",
+    },
+    config = function(_, opts)
+      local path = "~/.local/share/nvim/mason/packages/debugpy/venv/bin/python"
+      require("dap-python").setup(path)
+      require("core.utils").load_mappings("dap_python")
+    end,
+  },
+
+  {
+    "nvimtools/none-ls.nvim",
+    ft = {"python"},
+    opts = function()
+      return require "custom.configs.null-ls"
+    end,
+  },
+
+  --- RUST
+
+  {
+    "rust-lang/rust.vim",
+    ft = "rust",
+    init = function ()
+      vim.g.rustfmt_autosave = 1
+    end
+  },
+  {
+    "mrcjkb/rustaceanvim",
+    version = "^4",
+    ft = { "rust" },
+    dependencies = "neovim/nvim-lspconfig",
+    config = function()
+      require "custom.configs.rustaceanvim"
+    end
+  },
+  {
+    'saecki/crates.nvim',
+    ft = {"toml"},
+    config = function(_, opts)
+      local crates  = require('crates')
+      crates.setup(opts)
+      require('cmp').setup.buffer({
+        sources = { { name = "crates" }}
+      })
+      crates.show()
+      require("core.utils").load_mappings("crates")
+    end,
+  },
+  {
+    "rust-lang/rust.vim",
+    ft = "rust",
+    init = function ()
+      vim.g.rustfmt_autosave = 1
+    end
+  },
+  {
+    "theHamsta/nvim-dap-virtual-text",
+    lazy = false,
+    config = function(_, opts)
+      require("nvim-dap-virtual-text").setup()
+    end
+  },
+  {
+    "hrsh7th/nvim-cmp",
+    opts = function()
+      local M = require "plugins.configs.cmp"
+      M.completion.completeopt = "menu,menuone,noselect"
+      M.mapping["<CR>"] = cmp.mapping.confirm {
+        behavior = cmp.ConfirmBehavior.Insert,
+        select = false,
+      }
+      table.insert(M.sources, {name = "crates"})
+      return M
+    end
+  },
+
+
+  {
+    "williamboman/mason.nvim",
+    opts = {
+      ensure_installed = {
+        --- python
+        "black",
+        "debugpy",
+        "pyright",
+        "mypy",
+        "ruff-lsp",
+
+        --- c & c++
+        "clangd",
+        "clang-format",
+        "codelldb",
+
+        --- rust
+        "rust-analyzer",
+      },
+    },
+  },
+
+  {
+    "neovim/nvim-lspconfig",
+    config = function()
+      require "plugins.configs.lspconfig"
+      require "custom.configs.lspconfig"
+    end,
+  },
+
+  {
+		"nvim-treesitter/nvim-treesitter",
+		opts = {
+      ensure_installed = {
+        "python",
+        "c",
+        "cpp",
+        "lua",
+        "rust",
+      }
+    },
+	},
+}
+return plugins
