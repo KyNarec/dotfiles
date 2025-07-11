8c1e0cf627819a330f6d19077407fcf861574f29
KyNarec <jim48@gmx.de>
2025-07-11 09:25:46 +0200

    added neotest-java

--- .config/nvim/lua/custom/plugins.lua
+++ .config/nvim/lua/custom/plugins.lua
@@ -7,6 +7,36 @@ local plugins = {
   },
 
   {
+    "rcasia/neotest-java",
+    ft = "java",
+    dependencies = {
+      "mfussenegger/nvim-jdtls",
+      "mfussenegger/nvim-dap",           -- for the debugger
+      "rcarriga/nvim-dap-ui",            -- recommended
+      "theHamsta/nvim-dap-virtual-text", -- recommended
+    },
+  },
+  {
+    "nvim-neotest/neotest",
+    dependencies = {
+      "nvim-neotest/nvim-nio",
+      "nvim-lua/plenary.nvim",
+      "antoinemadec/FixCursorHold.nvim",
+      "nvim-treesitter/nvim-treesitter",
+    },
+    config = function()
+      require("neotest").setup({
+        adapters = {
+          require("neotest-java")({
+              junit_jar = nil, -- default: stdpath("data") .. /nvim/neotest-java/junit-platform-console-standalone-[version].jar
+  incremental_build = true
+          }),
+        },
+      })
+    end,
+  },
+
+  {
     "nvim-java/nvim-java",
     ft = { "java" },
     lazy = false,
@@ -189,7 +219,7 @@ local plugins = {
 
         -- Kotlin
         "kotlin-language-server",
-        
+
         -- Lua
         "lua-language-server"
       },
