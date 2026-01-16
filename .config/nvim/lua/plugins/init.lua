fdceb3f5a513edf8ef73a7b4bf7fb57dd1fedfe7
KyNarec <jim48@gmx.de>
2026-01-16 09:38:49 +0100

    bumped up rustaceanvim version

--- .config/nvim/lua/plugins/init.lua
+++ .config/nvim/lua/plugins/init.lua
@@ -124,7 +124,7 @@ return {
 
     {
         "mrcjkb/rustaceanvim",
-        version = "^5", -- Recommended
+        version = "^6", -- Recommended
         lazy = false,
         ft = { "rust" }, -- Load only for Rust files
     },
