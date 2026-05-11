951b0b5cd8ec2d0f583c10e9c90d5e7f1fe60d3a
KyNarec <jim48@gmx.de>
2026-05-11 14:40:28 +0200

    refactored lua files

--- .config/hypr/configs/monitors.lua
+++ .config/hypr/configs/monitors.lua
@@ -1,24 +1,24 @@
 hl.monitor({
     output = "desc:Acer Technologies ED273 A 0x01010101",
     mode = "1920x1080@119.98",
-    position = "3849x0",
+    position = "1920x0",
     scale = "1"
 })
 
 hl.monitor({
     output = "desc:Samsung Electric Company LF24T450F HNMT302120",
     mode = "1920x1080@60",
-    position = "1920x0",
+    position = "0x0",
     scale = "1"
 })
 
-hl.monitor({
-    output = "HDMI-A-1",
-    mode = "1680x1050@60",
-    position = "0x0",
-    scale = "1",
-    mirror = "eDP-1"
-})
+-- hl.monitor({
+--     output = "HDMI-A-1",
+--     mode = "1680x1050@60",
+--     position = "0x0",
+--     scale = "1",
+--     mirror = "eDP-1"
+-- })
 
 hl.monitor({
     output = "eDP-1",
